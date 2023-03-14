2d2ac4fede7ffefeb2ad0457dc877a892c232e73
RubTheKid <you@example.com>
2023-03-14 17:22:19 -0300

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,16 +13,111 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Snake
+namespace Snake;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+{
+{GridValue.Empty, Images.Empty },
+{GridValue.Snake, Images.Body },
+{GridValue.Food, Images.Food }
+};
+
+    private readonly int rows = 15, cols = 15;
+    private readonly Image[,] gridImages;
+    private GameState gameState;
+
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        gridImages = SetupGrid();
+        gameState = new GameState(rows, cols);
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        Draw();
+        await GameLoop();
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (gameState.GameOver)
         {
-            InitializeComponent();
+            return;
+        }
+
+        switch (e.Key)
+        {
+            case Key.Left:
+                gameState.ChangeDirection(Direction.Left);
+                break;
+            case Key.Right:
+                gameState.ChangeDirection(Direction.Right);
+                break;
+            case Key.Up:
+                gameState.ChangeDirection(Direction.Up);
+                break;
+            case Key.Down:
+                gameState.ChangeDirection(Direction.Down);
+                break;
         }
     }
+
+    private async Task GameLoop()
+    {
+        while (!gameState.GameOver)
+        {
+            await Task.Delay(100);
+            gameState.Move();
+            Draw();
+        }
+    }
+
+    private Image[,] SetupGrid()
+    {
+        Image[,] images = new Image[rows, cols];
+        GameGrid.Rows = rows;
+        GameGrid.Columns = cols;
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Image image = new Image
+                {
+                    Source = Images.Empty
+                };
+
+                images[row, col] = image;
+                GameGrid.Children.Add(image);
+            }
+        }
+        return images;
+    }
+
+    private void Draw()
+    {
+        DrawGrid();
+        ScoreText.Text = $"SCORE {gameState.Score}";
+    }
+
+    private void DrawGrid()
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                GridValue gridVal = gameState.Grid[row, col];
+                gridImages[row, col].Source = gridValToImage[gridVal];
+            }
+        }
+    }
+
 }
+
